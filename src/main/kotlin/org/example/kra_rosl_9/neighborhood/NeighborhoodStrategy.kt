984a9c2142079c0c3567daffae9240ff45e166a1
Alexandra Pominova <145675215+alfoll@users.noreply.github.com>
2026-01-10 03:40:29 +0300

    Clean up: remove old strategy and redundant gitignore

--- src/main/kotlin/org/example/kra_rosl_9/neighborhood/NeighborhoodStrategy.kt
+++
@@ -1,9 +0,0 @@
-//package org.example.kra_rosl_9.neighborhood
-//
-//import org.example.kra_rosl_9.logic.Grid
-//
-//interface NeighborhoodStrategy<T> {
-//
-//    // список состояний соседей для клетки (x, y)
-//    fun getNeighbors(grid: Grid<T>, x: Int, y: Int): List<T>
-//}
